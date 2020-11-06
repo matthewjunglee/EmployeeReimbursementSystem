26a24c462e10ea98250ca0c7da9b66fffd87125f
matthewjunglee <71738797+matthewjunglee@users.noreply.github.com>
2020-11-05 18:56:22 -0800

    update

--- sql/USER_ROLE.sql
+++
@@ -1,9 +0,0 @@
-drop table user_roles;
-create table user_roles (
-	user_role_id serial primary key,
-	user_role text not null
-)
-
-insert into user_roles values (1, 'finance_manager'), (2, 'employee');
-
-select * from user_roles;
